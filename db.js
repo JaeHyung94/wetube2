b9b95d49b059f588b302a94c29cda837ede24c76
JaeHyung94 <jhpark.main@gmail.com>
2020-08-26 23:24:26 +0900

    MVC model: view with PugJS 3

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 3234243,
+    title: `Awesome Video`,
+    description: `This is something I Love`,
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 12322,
+      name: "Jay",
+      email: "Jay@park.com",
+    },
+  },
+  {
+    id: 2758322,
+    title: `Fantastic Video`,
+    description: `This is something I Love`,
+    views: 27,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 12322,
+      name: "Jay",
+      email: "Jay@park.com",
+    },
+  },
+  {
+    id: 1784159,
+    title: `Nice Video`,
+    description: `This is something I Love`,
+    views: 77,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 12322,
+      name: "Jay",
+      email: "Jay@park.com",
+    },
+  },
+  {
+    id: 8894654,
+    title: `Super Video`,
+    description: `This is something I Love`,
+    views: 240,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 12322,
+      name: "Jay",
+      email: "Jay@park.com",
+    },
+  },
+];
